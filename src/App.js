482075ff10c27c6ef4c05dc2e20d798e61c148c9
emirmatik <emirmatik16@gmail.com>
2021-01-21 22:20:07 +0300

    loading screen

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import {MyContext} from './context';
 import Nav from './components/Nav';
 import Main from './components/Main';
+import LoadingScreen from './components/LoadingScreen';
 
 function App() {
-  let {pickedMusic} = React.useContext(MyContext);
+  let {loading, pickedMusic} = React.useContext(MyContext);
 
   return (
     <div className="container">
       <Nav />
       <Main />
+      {loading && <LoadingScreen />}
       <audio src={pickedMusic && pickedMusic.src} id="besmt">
         <source  type="audio/mpeg"></source>
       </audio>
---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function LoadingScreen(loading) {
+
+    return (
+        <div class="loading-screen" >
+            <div class="frame">
+                <div class="ball"></div>
+                <div class="ball"></div>
+                <div class="ball"></div>
+            </div>
+        </ div>
+    )
+}
+
+export default LoadingScreen;
