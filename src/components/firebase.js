7f27b17ab2a626ddee47a9f5b48326b0354d1f65
Emir <emirmatik16@gmail.com>
2025-04-06 17:08:06 +0300

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBW4_BnxhgDzPOD3hOa7hPkgGypZlGXjok",
-    authDomain: "spotical-f4e18.firebaseapp.com",
-    databaseURL: "https://spotical-f4e18.firebaseio.com",
-    projectId: "spotical-f4e18",
-    storageBucket: "spotical-f4e18.appspot.com",
-    messagingSenderId: "932822542084",
-    appId: "1:932822542084:web:a855bcf37d08e042cbd251"
+    apiKey: "xxx",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx"
   };
 
   firebase.initializeApp(firebaseConfig);
